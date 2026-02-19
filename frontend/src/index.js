62c688014147c44d861fe7f96035baf989141771
thushi379 <thushikaushalya1004@gmail.com>
2026-02-19 09:25:24 +0530

    initial commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./styles/main.css";
-import App from './App'; 
-
-function App() {
-  return (
-    <h1>Elecrical-shop</h1>
-  )
-}
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
